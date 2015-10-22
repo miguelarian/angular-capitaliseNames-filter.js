2cc38c72639a6509d8524940abbecc7aaa779420
Miguel Vela Romera <miguelarian@users.noreply.github.com>
2015-10-22 09:15:02 +0100

    bug fixing

--- capitalise-filter.js
+++ capitalise-filter.js
@@ -1,25 +1,25 @@
-angular.module('mymodule').filter('capitalise', ['$filter', function ($filter) 
+angular.module('app').filter('capitalise', ['$filter', function ($filter) 
+{
+    return function(items) 
     {
-        return function(items) 
+        if(!items)
         {
-            if(!items)
-            {
-                return items;
-            }
-
-            for(i=0; i<items.length; i++)
-            {
-                var words = items[i].Value.split(' ');
-
-                for(j=0; j<words.length; j++)
-                {
-                    words[j] = words[j].charAt(0).toUpperCase() + words[j].slice(1);
-                }
-
-                items[i].Value = words.join(' ');
-
-            }
             return items;
+        }
 
-        };
-    }]);
+        for(i=0; i<items.length; i++)
+        {
+            var words = items.split(' ');
+
+            for(j=0; j<words.length; j++)
+            {
+                words[j] = words[j].charAt(0).toUpperCase() + words[j].slice(1);
+            }
+
+            items = words.join(' ');
+
+        }
+        return items;
+
+    };
+}]);
